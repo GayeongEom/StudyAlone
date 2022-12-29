1a2b55e1b5c9081c8ca9e757db0d00c7dfbf03de
Gayeong Eom <um5605@naver.com>
2022-12-29 19:47:43 +0900

    1st class

---
+++ src/test/Test.java
@@ -0,0 +1,25 @@
+//주석, Comment
+// 	코드에 대한 설명을 작성할 때 사용 -> 자바 프로그램에 영향을 주지 않음
+
+package test;
+//패키지 선언코드 ; 현재 코드(파일)가 test라는 패키지에 포함되었음을 알림
+//	-> 첫글자 소문자
+
+public class Test {
+	//클래스 정의코드 -> 첫글자 대문자
+	// class ; 자바 프로그램을 구성하는 기본단위
+	
+	public static void main (String[] args) {
+		//메인 메소드(Main Method) 정의코드
+		//	method ; 프로그램의 동작기능을 구현하는 단위
+		//	main method ; 자바 프로그램의 시작점 (프로그램이 시작되는 위치)
+		
+		System.out.println("안녕하세요");
+		System.out.println("Hello World");
+		//콘솔에 문장을 출력하는 수행(실행)코드
+		// ln의 역할 ; 개행문자 / print는 출력후 줄바꿈x
+		
+		
+	} //main method end / 프로그램의 중단점(Terminate)
+
+} //Test class end
